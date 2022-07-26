8617754c3dc2cdc5265df9b1f07a1a01c7cec273
duellal <alexandriaduell@gmail.com>
2022-07-26 00:14:09 -0400

    Mostly styled with CSS - want to change position of the edit button to the upper right hand side of container

--- src/form.js
+++ src/form.js
@@ -22,7 +22,7 @@ export default function Form(props) {
             <label> Team Member Name:
                <input
                   type={'text'}
-                  placeholder='Team Member Name'
+                  placeholder='    Team Member Name'
                   value={values.name}
                   name='name'
                   onChange={onChange}
@@ -30,8 +30,8 @@ export default function Form(props) {
             </label>
             <label> Team Member Email:
                <input
-                  type={'text'}
-                  placeholder='Team Member Email'
+                  type={'email'}
+                  placeholder='    Team Member Email'
                   value={values.email}
                   name='email'
                   onChange={onChange}
